5b5ea1e99c5784c4fd903bbc40dc7c12ea1be7c8
wayne chang <wayne@neverfear.org>
2013-08-03 17:51:28 -0400

    graph header

--- lib/graph/graph.h
+++ lib/graph/graph.h
@@ -3,18 +3,37 @@
 #include <string.h>
 #include <curl/curl.h>
 
-/*Struct definitions*/
-struct string
-{
-    char *ptr;
-    size_t len;
-};
+typedef struct user {
+        unsigned long id;
+        const char *name;
+} user;
 
-/*Function definitions*/
-char **getFriends(char *access_token);
+/**
+ * access tokens are variable-length
+ * https://developers.facebook.com/docs/facebook-login/access-tokens/
+ **/
+typedef struct graph_session {
+        const user *me;
+        const char *access_token;
+} graph_session;
 
-/*Helpers*/
-size_t buildStringResponse(void *ptr, size_t size, size_t nmemb, struct string *s);
-void init_string(struct string *s);
-void destroy_string(struct string *s);
+typedef struct post {
+        unsigned long id;
+        const user *to, *from;
+        const char *message;
+} post;
+
+/**
+ * this returns a NULL-terminated list of pointers to const users
+ */
+const user **get_friends(const graph_session *, const user *, int);
+void free_users(const user **);
+void free_user(const user *);
+
+/**
+ * this returns a NULL-terminated list of pointers to const posts
+ */
+const post **get_posts(const graph_session *, const user *, int);
+void free_posts(const post **);
+void free_post(const post *);
 
