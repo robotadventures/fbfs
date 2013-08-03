10bcbba97eeeeb25b5802a07a2dc7be9baad5546
wayne chang <wayne@neverfear.org>
2013-08-03 18:40:53 -0400

    user features

--- lib/graph/user.c
+++ lib/graph/user.c
@@ -8,18 +8,36 @@ user *create_user(unsigned long id, const char *name)
         user *u = malloc(sizeof(user));
         if (u == NULL) {
                 perror("create_user malloc");
+                return NULL;
         }
-        char *n = malloc((size_t) strlen(name));
+        u->id = id;
+
+        char *n = malloc((size_t) (strlen(name) + 1));
+        if (n == NULL) {
+                perror("create_user malloc");
+                free(u);
+                return NULL;
+        }
+        strcpy(n, name);
         u->name = (const char *)n;
         
         return u;
 }
 
-void destroy_user(const user *u)
+void destroy_user(user *u)
 {
         if (u) {
                 free(u->name);
                 free(u);
         }
 }
+
+void destroy_users(users **us)
+{
+        while (us) {
+                destroy_user(*us);
+                us++;
+        }
+}
+
 
